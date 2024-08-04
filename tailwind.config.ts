843618b917e602790329dc5ecffa188d2854b2c4
Aaman <aaman.bhowmick21@gmail.com>
2024-08-04 20:49:14 +0530

    Step 1 completed - Starting point completed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,16 @@
 import type { Config } from "tailwindcss";
+import theme from "~/theme";
 
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: theme.base,
+        rainforest: theme.rainforest,
+        candy: theme.candy,
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
