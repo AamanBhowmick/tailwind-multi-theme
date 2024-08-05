fa16441862d60d95e6773a6881a6355ae2eb2aa1
Aaman <aaman.bhowmick21@gmail.com>
2024-08-05 07:23:01 +0530

    Step 2-Problem

---
+++ app/routes/step-2-problem/route.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Step2Problem() {
+  return (
+    <div className='flex gap-4 p-4'>
+      <div className="square"></div>
+      {/* TODO: Update the CSS to make the second square pink */}
+      <div className="square square--pink"></div>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import theme from "~/theme";
+const theme = require('./app/theme.tsx')
 
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
