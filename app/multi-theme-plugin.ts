3a6755d688bd7ff09da155ba1405831f8095b12c
Aaman <aaman.bhowmick21@gmail.com>
2024-08-22 08:21:38 +0530

    Done with the solution

--- app/multi-theme-plugin.ts
+++ app/multi-theme-plugin.ts
@@ -15,7 +15,7 @@ function getRgbChannels(hex: any) {
 // --secondary-some-nested-color: getRgbChannels('#0099aa')
 
 function getCssVariableDeclaration(input: any, path=[], output = {}) {
-  Object.entries(input).forEach(([key, value]) => {
+  Object.entries(input).forEach(([key, value]: any) => {
     const newPath = path.concat(key);
     if (typeof value !== 'string') {
       getCssVariableDeclaration(value, newPath, output)
@@ -67,8 +67,17 @@ function getCssVariableDeclaration(input: any, path=[], output = {}) {
 // }
 
 
-function getColorUtilitiesWithCssVariableReference(input: any) {
+function getColorUtilitiesWithCssVariableReference(input: any, path = []) {
   // It will replace the hard coded obj below
+  return Object.fromEntries(
+    Object.entries(input).map(([key, value]: any) => {
+      const newPath = path.concat(key);
+      if (typeof value !== 'string') {
+        return [key, getColorUtilitiesWithCssVariableReference(value, newPath)];
+      }
+      return [key, `rgb(var(--${newPath.join("-")}) / <alpha-value>)`]
+    })
+  )
 }
 
 
@@ -92,20 +101,7 @@ module.exports = plugin(function ({ addBase }: any) {
 {
   theme: {
     extend: {
-      colors: {
-        primary: {
-          50:  'rgb(var(--primary-50) / <alpha-value>)',
-          100: 'rgb(var(--primary-100) / <alpha-value>)',
-          200: 'rgb(var(--primary-200) / <alpha-value>)',
-          300: 'rgb(var(--primary-300) / <alpha-value>)',
-          400: 'rgb(var(--primary-400) / <alpha-value>)',
-          500: 'rgb(var(--primary-500) / <alpha-value>)',
-          600: 'rgb(var(--primary-600) / <alpha-value>)',
-          700: 'rgb(var(--primary-700) / <alpha-value>)',
-          800: 'rgb(var(--primary-800) / <alpha-value>)',
-          900: 'rgb(var(--primary-900) / <alpha-value>)',
-        }
-      },
+      colors: getColorUtilitiesWithCssVariableReference(themes.base),
     },
   },
 }
